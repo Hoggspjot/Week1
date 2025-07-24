e2f373608be4a9558be6f5931d2d348e8b37b943
Hoggspjot <vingisaar@gmail.com>
2025-07-24 17:41:53 +0300

    Strings and builders

---
+++ src/main/java/Solution.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String str = "Java".repeat(3);
+        System.out.println(str);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("J").append('a').append("v").append('a');
+        System.out.println(sb);
+
+    }
+}
