a8c2d03015493858273bce6c3dcae12549c25aa1
Hoggspjot <vingisaar@gmail.com>
2025-07-24 17:50:12 +0300

    builder

--- src/main/java/PatternBuilder.java
+++ src/main/java/PatternBuilder.java
@@ -1,4 +1,49 @@
 public class PatternBuilder {
+    public static void main(String[] args) {
+
+        Person person = new Person.Builder().setName("Bob").setAge(25).build();
+
+        System.out.println(person);
+
+    }
+
+}
 
 
+class Person {
+    private String name;
+    private int age;
+
+    public Person(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+    }
+
+
+    public static class Builder {
+        private String name;
+        private int age;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+        public Person build() {
+            return new Person(this);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
